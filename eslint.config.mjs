ce7de7c44d18443710b9efb811c8bf85173f02b8
Elias Wendm Atalay <eliaswendm.atalay@nttdata.com>
2026-02-22 07:07:52 +0100

    Chore: added Eslint & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+// Prettier integration
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // enable Prettier rules and disable conflicting ESLint rules
+  "plugin:prettier/recommended",
+  ...prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
